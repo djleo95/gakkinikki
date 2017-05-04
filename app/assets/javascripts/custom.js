a19380bf4b0f1eaf474d896b69ba82ce217da955
Strawberry <strawberrycsgo@gmail.com>
2017-05-04 12:22:46 +0700

    sprint3

---
+++ app/assets/javascripts/custom.js
@@ -0,0 +1,11 @@
+// $(document).ready(function(){
+//   $.ajax({
+//     type:"GET",
+//     url: "/images",
+//     dataType: "json"
+//   }).done(function(data) {
+//     $.each(data, function (index, element) {
+//       console.log(element)
+//     });
+//   })
+// })
